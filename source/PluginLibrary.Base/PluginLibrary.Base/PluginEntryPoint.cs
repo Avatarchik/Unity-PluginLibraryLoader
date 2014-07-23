a4e33856e548cbfe4669a071dc861937fb544a99
SlateNeon <slateneon@gmail.com>
2014-07-24 02:57:33 +0300

    Added example library

---
+++ source/PluginLibrary.Base/PluginLibrary.Base/PluginEntryPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+/* 
+ * All of these items are optional
+ * The assembly dll will be loaded anyway, even if it has no entry point but in that case it will most likely do nothing
+ */
+
+namespace PluginLibrary.Base    // Namespace in which PluginEntryPoint resides must be same as dll name
+{
+    public static class PluginEntryPoint    // Entry class
+    {
+        public static readonly Dictionary<string, string> info = new Dictionary<string, string>() { // Dictionary contains basic info
+            {"name","PluginBase"},
+            {"about","Base for a plugin that does nothing"},
+            {"version","1"},
+        };
+
+        public static void Init() {         // This function will be called when plugin is loaded
+            // Do stuff
+        }
+    }
+}
